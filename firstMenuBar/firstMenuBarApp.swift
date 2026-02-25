3a269aa5b17c7c63cfc66cf776910c5dd12b24c8
daniellberg <danielbergons@gmail.com>
2026-02-25 14:01:14 -0800

    Initial Commit

---
+++ firstMenuBar/firstMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstMenuBarApp.swift
+//  firstMenuBar
+//
+//  Created by Daniel Berg on 25/02/26.
+//
+
+import SwiftUI
+
+@main
+struct firstMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ firstMenuBarTests/firstMenuBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  firstMenuBarTests.swift
+//  firstMenuBarTests
+//
+//  Created by Daniel Berg on 25/02/26.
+//
+
+import Testing
+@testable import firstMenuBar
+
+struct firstMenuBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
