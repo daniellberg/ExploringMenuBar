de2b06f1a50c916084df7872cfb90a797d142e0c
daniellberg <danielbergons@gmail.com>
2026-02-25 22:14:37 -0800

    exploring MenuBarExtra()!! add: hello text, textfield, increment and decrement count button

--- firstMenuBar/firstMenuBarApp.swift
+++ firstMenuBar/firstMenuBarApp.swift
@@ -6,22 +6,50 @@
 //
 
 import SwiftUI
+import SwiftUI
 
 @main
-struct FirstMenuBarApp: App {
-
+struct MyApp: App {
+    @State private var textInput: String = "What are you counting?"
+    
     var body: some Scene {
-        MenuBarExtra("Demo", systemImage: "star.fill") {
+        
+        MenuBarExtra("My App Title", systemImage: "star.fill") {
+            VStack {
+                Text("Hello, Daniel! 👋🏻")
+                    .font(.system(size: 14, weight: .semibold, design: .monospaced))
+                    .padding()
+                
+                Divider()
+                
+                TextField("Enter text here", text: $textInput)
+                    .textFieldStyle(.roundedBorder)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                HStack{
+                    Button("+1"){
+                        print("Hi!")
+                    }
+                    .font(.headline)
+                    .foregroundColor(.white)
+//                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    
+                    Button("-1"){
+                        print("Hi!")
+                    }
+                    .font(.headline)
+                    .foregroundColor(.white)
+//                    .background(Color.blue)
+                    .cornerRadius(10)
 
-            Button("Hello 👋") {
-                print("Hello from menu bar")
-            }
+                }
+                .padding()
+                
 
-            Divider()
-
-            Button("Quit") {
-                NSApplication.shared.terminate(nil)
             }
         }
+        .menuBarExtraStyle(.window) // Use window style for a custom view
     }
 }
