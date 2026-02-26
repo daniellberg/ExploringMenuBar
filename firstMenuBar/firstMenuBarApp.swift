b2c98cf151b51e6b7594e722b92f1b356e227d63
daniellberg <danielbergons@gmail.com>
2026-02-25 23:10:05 -0800

    style: add some style and fix padding and unnecessary spacing add: isEnabled logic, lock/unlocked icon feature: lock/unlock textfield after click on lock symbol -> changes icon

--- firstMenuBar/firstMenuBarApp.swift
+++ firstMenuBar/firstMenuBarApp.swift
@@ -4,62 +4,60 @@
 //
 //  Created by Daniel Berg on 25/02/26.
 //
-
-import SwiftUI
 import SwiftUI
 
 @main
 struct MyApp: App {
     @State private var count = 0
+    @State private var isEnabled = false
     @State private var textInput: String = "What are you counting?"
 
-    
     var body: some Scene {
-        
-        MenuBarExtra("My App Title", systemImage: "star.fill") {
-            VStack {
+        MenuBarExtra("My App", systemImage: "star.fill") {
+            VStack(spacing: 10) {
                 Text("Hello, Daniel! 👋🏻")
                     .font(.system(size: 14, weight: .semibold, design: .monospaced))
-                    .padding()
-                
-                
-                TextField("Enter text here", text: $textInput)
-                    .textFieldStyle(.roundedBorder)
-                    .multilineTextAlignment(.center)
-                    .padding()
-                
-                Divider()
-                
-                Text("Count: \(count)")
-                    .font(.system(size: 14, weight: .semibold, design: .monospaced))
-                    .padding()
 
                 HStack{
-                    Button("+1"){
-                        count+=1
-                        print(count)
-                    }
-                    .font(.headline)
-                    .foregroundColor(.white)
-                    .controlSize(.large)
-//                    .background(Color.blue)
-                    .cornerRadius(10)
+                    TextField("Enter text here", text: $textInput)
+                        .disabled(isEnabled)
+                        .textFieldStyle(.roundedBorder)
+                        .multilineTextAlignment(.center)
+                        .frame(width: 200)
                     
-                    Button("-1"){
-                        count-=1
+                    Button(action: {
+                        isEnabled.toggle()
+                            }) {
+                                Image(systemName: isEnabled ? "lock.fill" : "lock.open.fill")
+                                    .padding(5)
+                            }
+                            .buttonStyle(PlainButtonStyle())
+                    
+                }
+                Divider()
+
+                Text("Count: \(count)")
+                    .font(.system(size: 14, weight: .semibold, design: .monospaced))
+
+                HStack(spacing: 10) {
+                    Button("+1") {
+                        count += 1
                         print(count)
                     }
-                    .font(.headline)
+                    .frame(width: 50, height: 30)
                     .foregroundColor(.white)
-                    .controlSize(.large)
-//                    .background(Color.blue)
-                    .cornerRadius(10)
+                    .cornerRadius(6)
 
+                    Button("-1") {
+                        count -= 1
+                        print(count)
+                    }
+                    .frame(width: 50, height: 30)
+                    .foregroundColor(.white)
+                    .cornerRadius(6)
                 }
-                .padding()
-                
-
             }
+            .padding(10)
         }
         .menuBarExtraStyle(.window)
     }
